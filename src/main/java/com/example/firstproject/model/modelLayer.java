87eb748f943d81ff09bd57fea215081640e95b9d
Pavan <aavulapavan16@gmail.com>
2024-08-27 14:38:35 -0700

    first commit

---
+++ src/main/java/com/example/firstproject/model/modelLayer.java
@@ -0,0 +1,16 @@
+package com.example.firstproject.model;
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import javax.swing.*;
+import java.net.SocketOption;
+
+@Component
+class modelLayer {
+
+    public void doSomething(){
+        System.out.println("In Service layer");
+    }
+
+}
